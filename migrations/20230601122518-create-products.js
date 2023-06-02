10a741888bef1a828ea02edc57676833a5b97fbf
praditya luthfi <pradityaluthfi45@gmail.com>
2023-06-02 13:45:12 +0700

    docs: fixing models database

---
+++ migrations/20230601122518-create-products.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      product_code: {
+        type: Sequelize.INTEGER
+      },
+      product_name: {
+        type: Sequelize.STRING
+      },
+      product_price: {
+        type: Sequelize.INTEGER
+      },
+      product_stock: {
+        type: Sequelize.INTEGER
+      },
+      expired_date: {
+        type: Sequelize.DATE
+      },
+      status_expired: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+  }
+};
---
+++ models/products.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      products.belongsTo(models.users, {
+        foreignKey: 'user_id'
+      });
+    }
+  }
+  products.init({
+    user_id: DataTypes.INTEGER,
+    product_code: DataTypes.INTEGER,
+    product_name: DataTypes.STRING,
+    product_price: DataTypes.INTEGER,
+    product_stock: DataTypes.INTEGER,
+    expired_date: DataTypes.DATE,
+    status_expired: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'products',
+  });
+  return products;
+};
